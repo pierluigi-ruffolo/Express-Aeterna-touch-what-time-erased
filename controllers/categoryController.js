3bbe96c60f13c3e3392d5ad6b7a38d3eb1b2ac50
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2026-02-03 11:26:12 +0100

    feat : routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,11 +1,35 @@
-function indexEras(req, res) {
-  res.send("indexeras");
+import connection from "../db/db.js"
+
+function indexEras(req, res, next) {
+
+  const query = `SELECT * FROM eras`;
+
+  connection.query(query, (err, result) => {
+    if (err) return next(err);
+    return res.json({
+      results: result,
+    });
+  });
 }
-function indexDiets(req, res) {
-  res.send("indexDiets");
+function indexDiets(req, res, next) {
+    const query = `SELECT * FROM diets`;
+
+  connection.query(query, (err, result) => {
+    if (err) return next(err);
+    return res.json({
+      results: result,
+    });
+  });
 }
-function indexPowerSources(req, res) {
-  res.send("powerSources");
+function indexPowerSources(req, res, next) {
+    const query = `SELECT * FROM power_sources`;
+
+  connection.query(query, (err, result) => {
+    if (err) return next(err);
+    return res.json({
+      results: result,
+    });
+  });
 }
 
 export { indexEras, indexDiets, indexPowerSources };
