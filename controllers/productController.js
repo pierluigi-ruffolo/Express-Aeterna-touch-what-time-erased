c416d4c2fbafba674fbcf0741a2aeead9442aca0
lucariggi92 <riggiluca92@gmail.com>
2026-02-05 09:55:28 +0100

    fix: query sqlRecommended

--- controllers/productController.js
+++ controllers/productController.js
@@ -109,20 +109,35 @@ function showProducts(req, res, next) {
 
     //correlati
     const sqlRecommended = `
-      (SELECT *, 'stessa_dieta' AS motivo FROM products WHERE diet_id = ? AND id != ? LIMIT 1)
-      UNION
-      (SELECT *, 'stessa_era' AS motivo FROM products WHERE era_id = ? AND id != ? LIMIT 1)
-      UNION
-      (SELECT *, 'stessa_energia' AS motivo FROM products WHERE power_source_id = ? AND id != ? LIMIT 1)
+      (SELECT *, 'stessa_dieta' AS motivo 
+      FROM products 
+      WHERE diet_id = ? 
+      AND id != ? 
+      LIMIT 1)
+      UNION ALL
+
+      (SELECT *, 'stessa_era' AS motivo 
+      FROM products 
+      WHERE era_id = ? 
+      AND id != ? 
+      AND id NOT IN
+      (SELECT id FROM products WHERE diet_id = ? AND id != ? LIMIT 1)LIMIT 1)
+
+      UNION ALL
+      (SELECT *, 'stessa_energia' AS motivo 
+      FROM products 
+      WHERE power_source_id = ? 
+      AND id != ? 
+      AND id NOT IN
+      (SELECT id FROM products WHERE diet_id = ? AND id != ? LIMIT 1) 
+      AND id NOT IN 
+      (SELECT id FROM products WHERE era_id = ? AND id != ? LIMIT 1) LIMIT 1)
     `;
 
     const params = [
-      product.diet_id,
-      product.id,
-      product.era_id,
-      product.id,
-      product.power_source_id,
-      product.id,
+      product.diet_id, product.id, 
+      product.era_id, product.id, product.diet_id, product.id,
+      product.power_source_id, product.id,  product.diet_id, product.id, product.era_id, product.id,
     ];
 
     connection.query(sqlRecommended, params, (err, recommendedResults) => {
